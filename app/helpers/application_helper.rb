5dafd8bf7abc9a2fe1daa1d94fa46af83325fbd9
青木宇宙 <soraaoki@aokisoranoMacBook-Air.local>
2020-10-29 16:03:51 +0900

    rubocop設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
-  def full_title(page_title = '')  # full_titleメソッドを定義
+  def full_title(page_title = '') # full_titleメソッドを定義
     base_title = 'カフェアプリ'
     if page_title.blank?
-      base_title  # トップページはタイトル「カフェアプリ」
+      base_title # トップページはタイトル「カフェアプリ」
     else
       "#{page_title} - #{base_title}" # トップ以外のページはタイトル「利用規約 - カフェアプリ」（例）
     end
