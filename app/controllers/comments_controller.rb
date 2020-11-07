3d50ce36ff393ad3f5b6fc5897e761ab77a0efcf
青木宇宙 <soraaoki@aokisoranoMacBook-Air.local>
2020-11-07 09:02:13 +0900

    通知を生成する

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,6 +7,12 @@ class CommentsController < ApplicationController
     @comment = @cafe.comments.build(user_id: current_user.id, content: params[:comment][:content])
     if !@cafe.nil? && @comment.save
       flash[:success] = "コメントを追加しました！"
+      if @user != current_user
+        @user.notifications.create(cafe_id: @cafe.id, variety: 2,
+                                   from_user_id: current_user.id,
+                                   content: @comment.content) # コメントは通知種別2
+        @user.update_attribute(:notification, true)
+      end
     else
       flash[:danger] = "空のコメントは投稿できません。"
     end
