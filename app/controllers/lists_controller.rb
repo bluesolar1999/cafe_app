1d93d172f4869fbb05376cacd2573d85bac190f6
青木宇宙 <soraaoki@aokisoranoMacBook-Air.local>
2020-11-07 14:32:13 +0900

    必要なメソッドの定義

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,8 +5,22 @@ class ListsController < ApplicationController
   end
 
   def create
+    @cafe = Cafe.find(params[:cafe_id])
+    @user = @cafe.user
+    current_user.list(@cafe)
+    respond_to do |format|
+      format.html { redirect_to request.referrer || root_url }
+      format.js
+    end
   end
 
   def destroy
+    list = List.find(params[:list_id])
+    @cafe = list.cafe
+    list.destroy
+    respond_to do |format|
+      format.html { redirect_to request.referrer || root_url }
+      format.js
+    end
   end
 end
