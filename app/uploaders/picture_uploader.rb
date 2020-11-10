36e812b6b072e8adbddb76a8308bceabbf7c2f6e
青木宇宙 <soraaoki@aokisoranoMacBook-Air.local>
2020-11-10 14:13:11 +0900

    AWS S3への画像保存

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,8 +1,11 @@
 class PictureUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
-  storage :file
-  # storage :fog
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
@@ -27,4 +30,6 @@ class PictureUploader < CarrierWave::Uploader::Base
   def extension_whitelist
     %w(jpg jpeg png)
   end
+
+
 end
